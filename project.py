01b4cbbc40e44ef0a31f13c1f0b059a34f0e53b9
Gensi Collin <99279784+Ikiriza@users.noreply.github.com>
2022-10-29 13:20:26 +0300

    Update project.py

--- project.py
+++ project.py
@@ -1,9 +1,9 @@
-secret_number = 3
+secret_number = 14
 num = 0
 
 while(num!=secret_number):
-    num = int(input("Guess the number:"))
-    if(num==secret_number ):
+    num = int(input("Please Enter the number:"))
+    if(num==secret_number):
         print("Well done, muggle! You are free now.")
     else:
         print("Ha ha! You're stuck in my loop!")
